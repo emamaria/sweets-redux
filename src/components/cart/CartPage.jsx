176b638133bdf2fd9088eb00dbd3b32e7497005c
emamaria <emascript@gmail.com>
2022-12-16 19:12:56 +0100

    total items and purchase

--- src/components/cart/CartPage.jsx
+++ src/components/cart/CartPage.jsx
@@ -6,12 +6,17 @@ const CartPage = () => {
    const cartState = useSelector(state => state.cartTasks)
 
   console.log("cartSatte", cartState)
+  const initialValue = 0;
+  let TotalPurchase = cartState.reduce( (acc, cur) => acc + cur.totalPrice, initialValue)
+  let TotalItems = cartState.reduce( (acc, cur) => acc + cur.amount, initialValue)
   return (
     <div className='cartPage_container'>
       { cartState.map((item, index) => {
         return <CartPageItem key={index} name={item.name} totalPrice={item.totalPrice} amount={item.amount} 
         price={item.price} category={item.category}/>
       } )}
+      { (TotalItems>0)&&<h1 className="totalItems">Total Items:{TotalItems}</h1>}
+     { (TotalPurchase>0)&&<h1 className="totalPurchase">Total purchase:{TotalPurchase.toFixed(2)}€</h1>}
     </div>
   )
 }
--- src/components/cart/CartPageItem.jsx
+++ src/components/cart/CartPageItem.jsx
@@ -20,8 +20,8 @@ const CartPageItem = ({price, totalPrice, name, amount, category}) => {
        {name && <img src={`/assets/${category}/${name.split(" ").join("_")}.jpg`} alt={name}/>}
        </div>
         <div className="cartDetail_container">
-            <h1>{name}</h1>
-            <h1>Price: {price}</h1>
+            <h1>{name[0].toUpperCase() + name.substring(1)}</h1>
+            <h1>Price: {price.toFixed(2)}</h1>
             <h1>Quantity: {amount}</h1>
             <h1>Total price: {totalPrice?.toFixed(2)}</h1>
             <div className="counter_container">
